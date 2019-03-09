ed550d0cf42019f71c01c63903ddb1599fccce80
Rahul255 <rahulmohan255@gmail.com>
2019-03-09 14:57:54 +0530

    magic8Ball.py

---
+++ chapter4/magic8Ball2.py
@@ -0,0 +1,3 @@
+import random
+messages=['It os certain','It is decidedly so','Yes definitely','Ask again later','Concentrate and ask again','My replay is no','Outlook not so good','Very doubtful']
+print(messages[random.randint(0,len(messages)-1)])
