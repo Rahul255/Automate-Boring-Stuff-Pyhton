0a4843113d1c32ba246cae1c5a4bd5264fe715a0
Rahul255 <rahulmohan255@gmail.com>
2019-03-09 15:24:30 +0530

    myPets.py

---
+++ chapter4/myPets.py
@@ -0,0 +1,9 @@
+myPets=['Zophie','Pooka','Fat-boy']
+print('enter a pet name')
+name=raw_input()
+if name in myPets:
+	print (name + ' is my pet')
+else:
+	print('I do not have a pet named ' + name)
+
+
